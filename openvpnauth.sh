8432a55a8c68f90b41c6478b4cafcdd759ee8ffe
carlrygart <carl@rygart.se>
2016-09-25 21:18:43 +0200

    Added feature to delete users. Bugfixes. Helptext.

--- openvpnauth.sh
+++ openvpnauth.sh
@@ -2,52 +2,66 @@
 
 #----------- Config parameters -----------#
 
-# Fetch openvpnauth path.
+# Fetch OpenVPNAuth path.
 my_path="`dirname \"$0\"`"              # relative
 my_path="`( cd \"$my_path\" && pwd )`"  # absolutized and normalized
-if [ -z "$my_path" ] ; then
+if [ -z "$my_path" ]; then
   # error; for some reason, the path is not accessible
   # to the script (e.g. permissions re-evaled after suid)
   exit 1  # fail
 fi
 
-db="$my_path/openvpnauth.db"
-logfile="$my_path/openvpnauth.log"
+db_file="$my_path/openvpnauth.db"
+log_file="$my_path/openvpnauth.log"
 
 #----------- Help message -----------#
-if [ $1 == "" ] || [ $1 == "help" ] || [ $1 == "-h" ]; then
-	echo "OpenVPN Authentication Script v1.0"
+if [ $1 == "" ] || [ $1 == "help" ] || [ $1 == "--help" ] || [ $1 == "-h" ]; then
+    echo "OpenVPN Authentication Script v1.0"
     echo "This is a script that enables OpenVPN authentication with username and password stored in a simple database."
-	echo ""
-	echo "Use with 'withauth-user-pass-verify via-file option'"
-	echo ""
-	echo "help - prints help"
-	echo "sha512 password - to compute password md5 checksum"
-	exit 0
+    echo ""
+    echo "Place in OpenVPN root folder.
+    echo "Enable this script by adding following to your OpenVPN config (server.conf):"
+    echo "script-security 2"
+    echo "auth-user-pass-verify /etc/openvpn/openvpnauth/openvpnauth.sh via-file"
+    echo "client-cert-not-required  # Optional"
+    echo "username-as-common-name"
+    echo ""
+    echo "Usage: bash openvpnauth.sh [options] [username]"
+    echo ""
+    echo "Options:"
+    echo "  -h, --help                     display this help message and exit"
+    echo "  -a, --add                      add user"
+    echo "  -d, --delete                   delete user"
+    exit 1
 fi
 
 #----------- Functions -----------#
 log() {
-    echo "$(date +'%Y-%m-%d %H:%M') - $1" >> $logfile
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" >> $log_file
 }
 
 adduser() {
     username=$1
 
-	#Check db for username duplicates.
-    unique_username=null
-    while [ $unique_username != "true" ]; do
-        if [ $username == "" ] || [ $unique_username != "null" ]; then
+    # Check db for username duplicates.
+    valid_username="null"
+    while [ $valid_username != "true" ]; do
+        if [ ! $username ] || [ $valid_username != "null" ]; then
             read -p "Username: " username
         fi
-        unique_username=true
-        while IFS='' read -r line || [[ -n "$line" ]]; do
-            known_uname=$(echo $line | cut -d ':' -f 1)
-            if [ $known_uname == $username ]; then
-                unique_username=false
-                echo "User '$username' already exists, try again."
-            fi
-        done < "$db"
+        valid_username=true
+
+        # Parameters for check uniqueness.
+        dollar_username='\$'$username'\$'
+        db_row=$(cat $db_file | grep $dollar_username)
+
+        if [[ $username == *"$"* ]]; then # Check invalid chars.
+            valid_username=false
+            echo "Username '$username' contains invalid chars, try again."
+        elif [[ $db_row ]]; then # Check uniqueness.
+            valid_username=false
+            echo "User '$username' already exists, try again."
+        fi
     done
 
     # Ask for password.
@@ -67,43 +81,77 @@ adduser() {
     hash_pass=$(mkpasswd -m sha-512 $password)
 
     # Log everything.
-    echo "$username:$hash_pass" >> openvpnauth.db
+    echo "\$$username$hash_pass" >> $db_file
     echo "New user: '$username' created"
     log "New user: '$username' created"
 }
 
+deluser() {
+    username=$1
+    if [ ! $username ]; then
+        echo "No valid username submitted"
+        exit 1
+    fi
+
+    dollar_username='\$'$username'\$'
+    db_row=$(cat $db_file | grep $dollar_username)
+
+    if [ $db_row ]; then
+        sed -i "\|$db_row|d" $db_file
+        echo "'$username' is now deleted."
+    else
+        echo "'$username' is not in database."
+    fi
+}
+
 #----------- Check input parameters -----------#
-if [ "$1" = "-a" ]; then
+if [ $1 = "-a" ] || [ $1 = "--add" ]; then
     adduser $2
-    exit 0
+    exit 1
+fi
+
+if [ $1 = "-d" ] || [ $1 = "--delete" ]; then
+    deluser $2
+    exit 1
 fi
 
 #----------- User/Pass validation -----------#
-# If no one of the past arguments were used, the script suppose it's a
+# If no one of the past arguments were used, the script assume it's a
 # validation.
 
 # Fetching username and password from arguments.
 username_and_password=$(cat $1)
 username=$(echo $username_and_password | awk '{print $1}')
 password=$(echo $username_and_password | awk '{print $2}')
+if [[ ! $username_and_password ]] || [[ ! $username ]] || [[ ! $password ]]; then
+    echo "No valid option"
+    exit 1
+fi
 
 # Fetching hash from db.
-db_salt=$(cat $db | grep $username: | cut -d ':' -f 2 | cut -d '$' -f 3)
-db_passhash=$(cat $db | grep $username: | cut -d ':' -f 2 | cut -d '$' -f 4)
+dollar_username='\$'$username'\$'
+db_user=$(cat $db_file | grep -e $dollar_username)
+if [ ! $db_user ]; then
+    echo "OpenVPN authentication failed: Login attempt for '$username'. User not i database"
+    log "OpenVPN authentication failed: Login attempt for '$username'. User not i database"
+    log "`env | awk '{printf "%s ", $0}'`"
+    exit 1
+fi
+db_salt=$(echo $db_user | cut -d '$' -f 4)
+db_passhash=$(echo $db_user | cut -d '$' -f 5)
 
 # Calculating hash from entered password.
 passhash_inc_salt=$(mkpasswd -m sha-512 -S $db_salt $password)
-passhash=$(echo $passhash_inc_salt | cut -d ':' -f 2 | cut -d '$' -f 4)
+passhash=$(echo $passhash_inc_salt | cut -d '$' -f 4)
 
 # Checking if the hashes matches
 if [ $passhash == $db_passhash ]; then
-    #echo "OpenVPN authentication successful: $username"
-	log "OpenVPN authentication successful: $username"
+    echo "OpenVPN authentication successful: User '$username'"
+	log "OpenVPN authentication successful: User '$username'"
 	exit 0
 else
-    #echo "OpenVPN authentication failed: $username"
-    log "OpenVPN authentication failed: $username"
-    enviroment="`env | awk '{printf "%s ", $0}'`"
-    log "$enviroment"
+    echo "OpenVPN authentication failed: Wrong password for user '$username'"
+    log "OpenVPN authentication failed: Wrong password for user '$username'"
+    log "`env | awk '{printf "%s ", $0}'`"
     exit 1
 fi
